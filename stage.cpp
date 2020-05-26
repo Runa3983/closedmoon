db301eb89d9b1dc2acbcea1fd13f5729f5793213
Runa3983 <takumi1217.2000@gmail.com>
2020-05-26 15:05:20 +0900

    stage.cppを追加

---
+++ stage.cpp
@@ -0,0 +1,22 @@
+#include "DxLib.h"		// Dxlibﾗｲﾌﾞﾗﾘを使用する
+#include "main.h"
+#include "stage.h"
+
+
+// 変数
+int StageImage;
+
+// ステージ情報の初期化
+void StageSystemInit(void)
+{
+	
+}
+
+void StageGameInit(void)
+{
+
+}
+void StageGameDraw(void)
+{
+	
+}
